271c1e22ade31b923d815a7eb8386b60b09987ed
Fabio Gianzanti <pardal@hotmail.com>
2024-11-12 17:59:53 -0300

    feat: check environment

--- src/check_env.py
+++ src/check_env.py
@@ -8,20 +8,22 @@ def show_env():
     print(f"Stable Baselines version: {stable_baselines3.__version__}")
 
     import mujoco
+    import os
 
     print(f"Mujoco version: {mujoco.__version__}")
 
-    env = gym.make("Humanoid-v5", render_mode="human")
+    xml_file = os.path.join(os.path.dirname(__file__), "robotis_op3", "scene.xml")
+    env = gym.make("Humanoid-v5", xml_file=xml_file, render_mode="human")
     observation, info = env.reset()
 
     episode_over = False
     while not episode_over:
-        action = (
-            env.action_space.sample()
-        )  # agent policy that uses the observation and info
+        action = env.action_space.sample()
         observation, reward, terminated, truncated, info = env.step(action)
-
         episode_over = terminated or truncated
+
+    # while True:
+    #     env.render()
 
     env.close()
 
